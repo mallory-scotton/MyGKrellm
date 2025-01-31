176fa991c3b8a4e4f5038f6121af5cf6d0838d11
mallory-scotton <mallory.scotton@epitech.eu>
2025-01-31 16:55:16 +0100

    add: adding modules

--- source/TextMonitor.cpp
+++ source/TextMonitor.cpp
@@ -31,7 +31,9 @@
 #include <iostream>
 
 ///////////////////////////////////////////////////////////////////////////////
-TextMonitor::TextMonitor(void) : IMonitorDisplay()
+TextMonitor::TextMonitor(void)
+    : IMonitorDisplay()
+    , m_selected(0)
 {}
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -41,108 +43,116 @@ TextMonitor::~TextMonitor()
 ///////////////////////////////////////////////////////////////////////////////
 bool TextMonitor::getInput(void)
 {
-    char c = getch();
+    int c = getch();
 
-    if (c == 'u') m_user.setEnabled(!m_user.isEnabled());
-    if (c == 'm') m_memory.setEnabled(!m_memory.isEnabled());
-    if (c == 'o') m_os.setEnabled(!m_os.isEnabled());
-    if (c == 'p') m_cpu.setEnabled(!m_cpu.isEnabled());
-    if (c == 'n') m_network.setEnabled(!m_network.isEnabled());
-    if (c == 't') m_time.setEnabled(!m_time.isEnabled());
-    if (c == 'q') return (false);
+    if (c == (int)'u') m_user.setEnabled(!m_user.isEnabled());
+    if (c == (int)'m') m_memory.setEnabled(!m_memory.isEnabled());
+    if (c == (int)'o') m_os.setEnabled(!m_os.isEnabled());
+    if (c == (int)'p') m_cpu.setEnabled(!m_cpu.isEnabled());
+    if (c == (int)'n') m_network.setEnabled(!m_network.isEnabled());
+    if (c == (int)'t') m_time.setEnabled(!m_time.isEnabled());
+    if (c == (int)'q') return (false);
+    if (c == KEY_UP && m_selected > 0) { m_selected--; clear(); }
+    if (c == KEY_DOWN && m_selected < 4) { m_selected++; clear(); }
 
     return (true);
 }
 
 ///////////////////////////////////////////////////////////////////////////////
-void TextMonitor::printUserModule(void) const
+void TextMonitor::printNavBar(WINDOW* Wcontent) const
 {
-    WINDOW *Cwindow = subwin(stdscr, 7, 50, 1, 4);
+    static const char* menuItems[] = {
+        "Information   ", "Processor     ",
+        "Memory        ", "Network       ",
+        "Credits       "
+    };
+    static const char* contentItems[] = {
+        "Information", "Processor", "Memory", "Network", "Credits"
+    };
+    WINDOW *Wnavbar = subwin(stdscr, m_height - 2, 19, 1, 2);
 
-    if (m_user.isEnabled()) {
-        box(Cwindow, ACS_VLINE, ACS_HLINE);
-        wattron(Cwindow, A_BOLD);
-        mvwprintw(Cwindow, 0, 2, " User ");
-        mvwprintw(Cwindow, 2, 14," %s", m_user.getUserName().c_str());
-        mvwprintw(Cwindow, 4, 14," %s", m_user.getHostName().c_str());
-        wattroff(Cwindow, A_BOLD);
-        wrefresh(Cwindow);
-    } else {
-        wbkgd(Cwindow, COLOR_PAIR(2));
-        wrefresh(Cwindow);
+    box(Wnavbar, ACS_VLINE, ACS_HLINE);
+    mvwprintw(Wnavbar, 0, 4, " Navigation ");
+
+    mvwprintw(Wnavbar, m_height - 6, 3, "/\\ Move up");
+    mvwprintw(Wnavbar, m_height - 5, 3, "\\/ Move down");
+
+    for (size_t i = 0; i < sizeof(menuItems) / sizeof(menuItems[0]); i++) {
+        if (i == m_selected) {
+            wattron(Wnavbar, A_REVERSE);
+            mvwprintw(Wnavbar, 2 + i * 2, 3, "%s", menuItems[i]);
+            wattroff(Wnavbar, A_REVERSE);
+        } else {
+            mvwprintw(Wnavbar, 2 + i * 2, 3, "%s", menuItems[i]);
+        }
     }
-}
-
-void TextMonitor::printDateModule(void) const
-{
-    WINDOW *Cwindow = subwin(stdscr, 5, 50, 8, 4);
-
-    start_color();
-    if (m_time.isEnabled()) {
-        box(Cwindow, ACS_VLINE, ACS_HLINE);
-        mvwprintw(Cwindow, 0, 2, " Time ");
-        mvwprintw(Cwindow, 2, 12," %s", m_time.getTime().c_str());
-        wrefresh(Cwindow);
-    } else {
-        wbkgd(Cwindow, COLOR_PAIR(2));
-        wrefresh(Cwindow);
-    }
-}
-
-void TextMonitor::printOsModule(void) const
-{
-    WINDOW *Cwindow = subwin(stdscr, 7, 50, 13, 4);
-
-    start_color();
-    if (m_os.isEnabled()) {
-        box(Cwindow, ACS_VLINE, ACS_HLINE);
-        mvwprintw(Cwindow, 0, 2, " Operating System ");
-        mvwprintw(Cwindow, 2, 18," %s", m_os.getOs().c_str());
-        mvwprintw(Cwindow, 4, 13," %s",  m_os.getKernel().c_str());
-        wrefresh(Cwindow);
-    }  else {
-        wbkgd(Cwindow, COLOR_PAIR(2));
-        wrefresh(Cwindow);
-    }
-}
-
-void TextMonitor::printNetworkModule(void) const
-{
-    WINDOW *Cwindow = subwin(stdscr, 7, 50, 47, 4);
-
-    if (m_network.isEnabled()) {
-        int up = m_network.getUp() / 1048576;
-        box(Cwindow, ACS_VLINE, ACS_HLINE);
-        mvwprintw(Cwindow, 0, 2, " Network ");
-        start_color();
-        mvwprintw(Cwindow, 2, 4,"UPLOAD: %d MB/s", up);
-        up = m_network.getDown() / 1048576;
-        mvwprintw(Cwindow, 4, 4,"DOWNLOAD: %d MB/s", up);
-        wrefresh(Cwindow);
-    } else {
-        wbkgd(Cwindow, COLOR_PAIR(2));
-        wrefresh(Cwindow);
-    }
-}
-
-void TextMonitor::printHelp(void) const
-{
-    WINDOW *Cwindow = subwin(stdscr, 15, 30, 15, 60);
-
-    start_color();
-    box(Cwindow, ACS_VLINE, ACS_HLINE);
-    mvwprintw(Cwindow, 0, 2, " Help ");
-    mvwprintw(Cwindow, 2, 4, "%s", "Hide User data:\t'u'");
-    mvwprintw(Cwindow, 4, 4, "%s", "Hide Time:\t\t't'");
-    mvwprintw(Cwindow, 6, 4, "%s", "Hide Os data:\t'o'");
-    mvwprintw(Cwindow, 8, 4, "%s", "Hide Cpu data:\t'p'");
-    mvwprintw(Cwindow, 10, 4, "%s", "Hide Memory data:\t'm'");
-    mvwprintw(Cwindow, 12, 4, "%s", "Hide Network data:\t'n'");
-    wrefresh(Cwindow);
+    wrefresh(Wnavbar);
+    mvwprintw(Wcontent, 0, 4, " %s ", contentItems[m_selected]);
 }
 
 ///////////////////////////////////////////////////////////////////////////////
-int TextMonitor::loop(void)
+void TextMonitor::printInformation(WINDOW* Wcontent) const
+{
+    (void)Wcontent;
+    {
+        WINDOW* Wtime = subwin(stdscr, 5, m_width - 25, 3, 23);
+
+        if (!m_time.isEnabled()) wattron(Wtime, WA_DIM);
+        box(Wtime, ACS_VLINE, ACS_HLINE);
+        mvwprintw(Wtime, 0, 2, " [t] Time Module ");
+        mvwprintw(Wtime, 2, 3, "Current Date: %s", m_time.getTime().c_str());
+        if (!m_time.isEnabled()) wattroff(Wtime, WA_DIM);
+    }
+
+    {
+        WINDOW* Wuser = subwin(stdscr, 7, m_width - 25, 9, 23);
+
+        if (!m_user.isEnabled()) wattron(Wuser, WA_DIM);
+        box(Wuser, ACS_VLINE, ACS_HLINE);
+        mvwprintw(Wuser, 0, 2, " [u] User Module ");
+        mvwprintw(Wuser, 2, 3, "User Name: %s", m_user.getUserName().c_str());
+        mvwprintw(Wuser, 4, 3, "Host Name: %s", m_user.getHostName().c_str());
+        if (!m_user.isEnabled()) wattroff(Wuser, WA_DIM);
+    }
+
+    {
+        WINDOW* Wos = subwin(stdscr, 7, m_width - 25, 17, 23);
+
+        if (!m_os.isEnabled()) wattron(Wos, WA_DIM);
+        box(Wos, ACS_VLINE, ACS_HLINE);
+        mvwprintw(Wos, 0, 2, " [o] Operating System Module ");
+        mvwprintw(Wos, 2, 3, "Os Name: %s", m_os.getOs().c_str());
+        mvwprintw(Wos, 4, 3, "Kernel Name: %s", m_os.getKernel().c_str());
+        if (!m_os.isEnabled()) wattroff(Wos, WA_DIM);
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////////
+void TextMonitor::printProcessor(WINDOW* Wcontent) const
+{
+    (void)Wcontent;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+void TextMonitor::printMemory(WINDOW* Wcontent) const
+{
+    (void)Wcontent;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+void TextMonitor::printNetwork(WINDOW* Wcontent) const
+{
+    (void)Wcontent;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+void TextMonitor::printCredits(WINDOW* Wcontent) const
+{
+    (void)Wcontent;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+static void initNcurses(void)
 {
     initscr();
     noecho();
@@ -154,13 +164,35 @@ int TextMonitor::loop(void)
     use_default_colors();
     init_pair(10, COLOR_RED, -1);
     init_pair(11, COLOR_GREEN, -1);
+}
+
+///////////////////////////////////////////////////////////////////////////////
+int TextMonitor::loop(void)
+{
+    int oldW = 0, oldH = 0;
+
+    initNcurses();
     while (getInput()) {
+        getmaxyx(stdscr, m_height, m_width);
+        if (oldW != m_width || oldH != m_height) {
+            clear();
+            oldW = m_width;
+            oldH = m_height;
+        }
+
+        WINDOW* Wcontent = subwin(stdscr, m_height - 2, m_width - 21, 1, 21);
+        box(Wcontent, ACS_VLINE, ACS_HLINE);
+
         refresh();
-        printUserModule();
-        printDateModule();
-        printOsModule();
-        printNetworkModule();
-        printHelp();
+        printNavBar(Wcontent);
+
+        if (m_selected == 0) printInformation(Wcontent);
+        if (m_selected == 1) printProcessor(Wcontent);
+        if (m_selected == 2) printMemory(Wcontent);
+        if (m_selected == 3) printNetwork(Wcontent);
+        if (m_selected == 4) printCredits(Wcontent);
+
+        wrefresh(Wcontent);
     }
     curs_set(1);
     nocbreak();
